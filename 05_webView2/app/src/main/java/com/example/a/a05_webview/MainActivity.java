46807c619ca11448fddbacb05d73ddc9bd66ff26
Your Name <you@example.com>
2016-07-25 18:09:19 +0900

    webView

--- 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
+++ 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.a.a05_webview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.naver.com");
+    }
+
+    public void onBtnOkClick(View v){
+
     }
 }
