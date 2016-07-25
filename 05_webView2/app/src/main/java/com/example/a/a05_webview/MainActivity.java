17d9629d2af6713a3167881c465af0ed065cfe60
Your Name <you@example.com>
2016-07-25 18:20:43 +0900

    webView

--- 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
+++ 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a.a05_webview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,9 +8,25 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+
+    class MyWebViewClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            super.onPageFinished(view, url);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
