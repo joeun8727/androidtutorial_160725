78210cb04cb919511b770bc19bbe6da2c57d3426
Your Name <you@example.com>
2016-07-25 17:44:35 +0900

    widget2

--- 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
+++ 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         CheckBox myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
+        myCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "checkBox checked: " + isChecked , Toast.LENGTH_SHORT).show();
+            }
+        });
+        RadioGroup myRadioGroup = (RadioGroup)findViewById(R.id.myRadioGroup);
+        myRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio1 :
+                        Toast.makeText(MainActivity.this, "radio1 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio2 :
+                        Toast.makeText(MainActivity.this, "radio2 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio3 :
+                        Toast.makeText(MainActivity.this, "radio3 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
