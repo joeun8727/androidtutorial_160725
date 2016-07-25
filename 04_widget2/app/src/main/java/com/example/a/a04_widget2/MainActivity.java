1d885c6cc0693257d004f25ffd7abc53f17896d7
Your Name <you@example.com>
2016-07-25 17:20:35 +0900

    widget2

--- 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
+++ 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a.a04_widget2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOk = (Button)findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "ok button click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        CheckBox myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
     }
 }
