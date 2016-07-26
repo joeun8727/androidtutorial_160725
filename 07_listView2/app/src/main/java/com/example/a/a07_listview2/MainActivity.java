db1ca09094e5e039c26dc6aacc646515bba5757a
Your Name <you@example.com>
2016-07-26 10:54:05 +0900

    listView2

--- 07_listView2/app/src/main/java/com/example/a/a07_listview2/MainActivity.java
+++ 07_listView2/app/src/main/java/com/example/a/a07_listview2/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.a.a07_listview2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    class MyData{
+        public MyData(int imgId, String strTitle, String strDesc) {
+            this.imgId = imgId;
+            this.strTitle = strTitle;
+            this.strDesc = strDesc;
+        }
+
+        int imgId;
+        String strTitle;
+        String strDesc;
+    }
+    ArrayList<MyData> list = new ArrayList<MyData>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i =0; i< 30; i++){
+            list.add(new MyData(R.mipmap.ic_launcher,"title:" + i , "desc:" + i));
+        }
+
     }
 }
