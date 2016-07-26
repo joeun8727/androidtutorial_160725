6c4c9d04b0097bc57c9823f6a6690acc584fa516
Your Name <you@example.com>
2016-07-26 14:44:40 +0900

    xml

--- 10_xml/app/src/main/java/com/example/a/a10_xml/MainActivity.java
+++ 10_xml/app/src/main/java/com/example/a/a10_xml/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.a.a10_xml;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Document;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    class MyDomParser extends AsyncTask<String , Void, Document>{
+
+        @Override
+        protected Document doInBackground(String... params) {
+
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        MyDomParser task = new MyDomParser();
+        task.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1154052000");
     }
 }
