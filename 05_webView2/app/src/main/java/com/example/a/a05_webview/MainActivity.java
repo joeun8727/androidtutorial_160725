642537001a20e011054bfdb9276900171d8165cf
Your Name <you@example.com>
2016-07-26 09:45:54 +0900

    webView

--- 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
+++ 05_webView2/app/src/main/java/com/example/a/a05_webview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a.a05_webview;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,21 +9,24 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
 
+    ProgressDialog dlg;
+
     class MyWebViewClient extends WebViewClient{
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            dlg.show();
             super.onPageStarted(view, url, favicon);
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
-            Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "onPageStarted", Toast.LENGTH_SHORT).show();
+            dlg.dismiss();
             super.onPageFinished(view, url);
         }
     }
@@ -31,16 +35,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dlg = new ProgressDialog(MainActivity.this);
+
         webView = (WebView)findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.naver.com");
+        webView.loadUrl("http://www.daum.net");
     }
 
     public void onBtnOkClick(View v){
         EditText editURL = (EditText)findViewById(R.id.editURL);
         String url = editURL.getText().toString();
         webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
