b7c9be5988752c5b6c3417842461f9b7c207e827
Your Name <you@example.com>
2016-07-27 16:18:40 +0900

    service

--- 16_service/app/src/main/java/com/example/a/a16_service/MyService.java
+++ 16_service/app/src/main/java/com/example/a/a16_service/MyService.java
@@ -23,7 +23,7 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("service", "onStartCommand");
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Override
