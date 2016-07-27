5bbf7923b1cf5a260a1bc9738e50c0dc7b478bd7
Your Name <you@example.com>
2016-07-27 11:07:55 +0900

    sqlLite

--- 13_sqlLite/app/src/main/java/com/example/a/a13_sqllite/MainActivity.java
+++ 13_sqlLite/app/src/main/java/com/example/a/a13_sqllite/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestDBHandler dbHandler = new TestDBHandler(this);
+        dbHandler.insert("홍길동", 10, "서울");
+        dbHandler.insert("가나다", 11, "인천");
+        dbHandler.insert("사람", 12, "부산");
+
+        dbHandler.delete("가나다");
+        dbHandler.update("사람", 13);
+        dbHandler.selectAll();
     }
 }
