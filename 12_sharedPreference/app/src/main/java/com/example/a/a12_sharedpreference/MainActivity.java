728deaf668e3bdbad1ecaf574d553897aa7c22bf
Your Name <you@example.com>
2016-07-27 10:35:59 +0900

    sharedPreference

---
+++ 12_sharedPreference/app/src/main/java/com/example/a/a12_sharedpreference/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a.a12_sharedpreference;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = getSharedPreferences("MySetting", 0);
+        String value = preferences.getString("name", "NO DATA");
+        Toast.makeText(MainActivity.this, "VALUE : " + value, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences preferences = getSharedPreferences("MySetting", 0);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("name", "홍길동");
+        editor.commit();
+    }
+}
