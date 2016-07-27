884c7f1dfc68f8c61e0d0fc0f75618a7a48e5ece
Your Name <you@example.com>
2016-07-27 18:24:25 +0900

    br2

--- 19_broadCasterReceiver/app/src/main/java/com/example/a/a19_broadcasterreceiver/MainActivity.java
+++ 19_broadCasterReceiver/app/src/main/java/com/example/a/a19_broadcasterreceiver/MainActivity.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,23 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            Toast.makeText(context, "battery : " + level, Toast.LENGTH_SHORT).show();
+            if(intent.getAction().equals("ACTION_BATTERY_LOW")){
+                Toast.makeText(context, "battery : row Battery", Toast.LENGTH_SHORT).show();
+            }else if(intent.getAction().equals("ACTION_BATTERY_CHANGED")){
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Toast.makeText(context, "battery : " + level, Toast.LENGTH_SHORT).show();
+            }else if(intent.getAction().equals("abcdefg")){
+                Toast.makeText(context, "abcdefg", Toast.LENGTH_SHORT).show();
+            }
         }
     };
+
+    public void onBtnClick(View v){
+        Intent intent = new Intent("abcdefg");
+        sendBroadcast(intent);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction("abcdefg");
         registerReceiver(receiver, filter);
 
     }
