03657e1c1ca0dc12cf27454c2ae95e926fa1fcbb
Your Name <you@example.com>
2016-07-28 11:12:16 +0900

    camera

--- 21_camera/app/src/main/java/com/example/a/a21_camera/MainActivity.java
+++ 21_camera/app/src/main/java/com/example/a/a21_camera/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.a.a21_camera;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private static final int TAKE_PICUTURE = 100;
     public void onBtnClick(View v){
+        String path = Environment.getExternalStorageDirectory() + "/t21.jpg";
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+        startActivityForResult(intent, TAKE_PICUTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TAKE_PICUTURE){
+            if(resultCode == RESULT_OK){
+                String path = Environment.getExternalStorageDirectory() + "/t21.jpg";
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
     }
 }
