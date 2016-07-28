babe5f58904dd3182d891aa86e01a64af9245256
Your Name <you@example.com>
2016-07-28 12:27:02 +0900

    -notification

---
+++ 22_notification/app/src/main/java/com/example/a/a22_notification/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.a.a22_notification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //수동으로 삭제
+        /*NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.cancel(1234);*/
+    }
+}
