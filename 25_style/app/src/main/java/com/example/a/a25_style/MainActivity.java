e5bc1af2670de6b943ad964959794e4d0013f4d8
Your Name <you@example.com>
2016-07-29 11:13:26 +0900

    style

--- 25_style/app/src/main/java/com/example/a/a25_style/MainActivity.java
+++ 25_style/app/src/main/java/com/example/a/a25_style/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.a.a25_style;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView selectedTextView, workingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectedTextView = (TextView) findViewById(R.id.selectedTextView);
+        workingTextView = (TextView) findViewById(R.id.workingTextView);
+
+        Button zeroButton = (Button) findViewById(R.id.zeroBtn);
+        Button oneButton = (Button) findViewById(R.id.oneBtn);
+        Button enterButton = (Button) findViewById(R.id.enterBtn);
     }
+
+
 }
