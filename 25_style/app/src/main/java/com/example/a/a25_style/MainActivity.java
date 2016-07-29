58c12709839dba760d5de680956beb534e289df6
Your Name <you@example.com>
2016-07-29 11:54:52 +0900

    style

--- 25_style/app/src/main/java/com/example/a/a25_style/MainActivity.java
+++ 25_style/app/src/main/java/com/example/a/a25_style/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a.a25_style;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,10 +16,34 @@ public class MainActivity extends AppCompatActivity {
         selectedTextView = (TextView) findViewById(R.id.selectedTextView);
         workingTextView = (TextView) findViewById(R.id.workingTextView);
 
+        View.OnClickListener numberListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Button button = (Button)view;
+                String working = workingTextView.getText().toString();
+                String str = button.getText().toString();
+                if(working.equals("0")){
+                    workingTextView.setText(str);
+                }else{
+                    workingTextView.setText(working + str);
+                }
+            }
+        };
+
         Button zeroButton = (Button) findViewById(R.id.zeroBtn);
         Button oneButton = (Button) findViewById(R.id.oneBtn);
+
+        zeroButton.setOnClickListener(numberListener);
+        oneButton.setOnClickListener(numberListener);
+
         Button enterButton = (Button) findViewById(R.id.enterBtn);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String working = workingTextView.getText().toString();
+                selectedTextView.setText(working);
+                workingTextView.setText("0");
+            }
+        });
     }
-
-
 }
